3538fd53ae8c1685fd83aa4f173e40adcb7c34e7
Emeka Igwe <igweemekae@MacBookPro.home>
2024-11-29 20:26:17 +0000

    developed two new applications

---
+++ Python/bandname-generator/README.md
@@ -0,0 +1,6 @@
+This is mini project aimed at generating a unique Bandname with the folliwng steps
+
+1. create a greeting for your program
+2. Ask the user for the city that they grew up in and store it in a variable
+3. Ask the user for the name of a pet and store in a variable
+4. Combine the name of ther city and pet and show them their band name
---
+++ Python/bandname-generator/bandname.py
@@ -0,0 +1,12 @@
+#create a greeting for your program
+print("Hey! welcome to the name generator game!!!")
+
+#Ask the user for the city they grew up
+city = input("what city did you grow up?\n ")
+
+#Ask the use for the name of their pet?
+petname = input("what is the name of your pet?\n ")
+
+#combine the city name and pet name and generate the band name
+bandname = city + " " +  petname
+print(bandname)
